f74a6e64b8df53fd74bdf187815aef0bd0475f6c
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-16 19:27:42 +0000

    add

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,20 +1,10 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN_H
 
-/**
- * main - check the code.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	int r;
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
 
-	r = _islower('H');
-	_putchar(r + '0');
-	r = _islower('o');
-	_putchar(r + '0');
-	r = _islower(108);
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0);
-}
+#endif
+
