020f4c765316e107f730dc9f78661256326d7280
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-16 19:24:33 +0000

    fix 3-islower.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,8 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int r;
 
-#endif
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
