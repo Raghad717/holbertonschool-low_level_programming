87a391fc82e7c966311d52767bc30b195017f168
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-25 14:21:02 +0000

    add

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,10 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdio.h>
-#include <math.h>
-
-/* Function prototypes would go here if we had any */
+int _putchar(char c);
+void print_number(int n);
 
 #endif
-
