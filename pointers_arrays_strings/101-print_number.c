a63e526b013aeac7c64c658efd29beb61e778647
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-25 14:53:34 +0000

    add

---
+++ pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using only _putchar
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+    unsigned int num, divisor, digit;
+
+    if (n < 0)
+    {
+        _putchar('-');
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
+
+    divisor = 1;
+    while (num / divisor > 9)
+    {
+        divisor *= 10;
+    }
+
+    while (divisor > 0)
+    {
+        digit = num / divisor;
+        _putchar(digit + '0');
+        num %= divisor;
+        divisor /= 10;
+    }
+}
+
