4c17f99b866574c1e786b759e08d4fc71c0a84b1
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-25 14:58:39 +0000

    add

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -6,30 +6,30 @@
  */
 void print_number(int n)
 {
-    unsigned int num, divisor, digit;
+	unsigned int num, divisor, digit;
 
-    if (n < 0)
-    {
-        _putchar('-');
-        num = -n;
-    }
-    else
-    {
-        num = n;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
 
-    divisor = 1;
-    while (num / divisor > 9)
-    {
-        divisor *= 10;
-    }
+	divisor = 1;
+	while (num / divisor > 9)
+	{
+		divisor *= 10;
+	}
 
-    while (divisor > 0)
-    {
-        digit = num / divisor;
-        _putchar(digit + '0');
-        num %= divisor;
-        divisor /= 10;
-    }
+	while (divisor > 0)
+	{
+		digit = num / divisor;
+		_putchar(digit + '0');
+		num %= divisor;
+		divisor /= 10;
+	}
 }
 
