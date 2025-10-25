912a090cbb172f3afa3540d953c15d52fdf2f4d8
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-25 14:19:52 +0000

    add

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer using only _putchar
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+    unsigned int num, divisor, digit;
+
+    /* Handle negative numbers */
+    if (n < 0)
+    {
+        _putchar('-');
+        num = -n;
+    }
+    else
+    {
+        num = n;
+    }
+
+    /* Find the appropriate divisor */
+    divisor = 1;
+    while (num / divisor > 9)
+        divisor *= 10;
+
+    /* Print each digit */
+    while (divisor != 0)
+    {
+        digit = num / divisor;
+        _putchar(digit + '0');
+        num %= divisor;
+        divisor /= 10;
+    }
+}
+
