6810607bafbac1b9331f45e27b89a7b04581156e
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-25 14:08:05 +0000

    add

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,16 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-int _putchar(char c);
+#include <stdio.h>
+#include <math.h>
+
+/* Function prototypes would go here if we had any */
 
 #endif
+
