0ce8c40768f55a06ebdbc2eb05779efeb89ea986
Raghad717 <r4gh4dalmalki@gmail.com>
2025-10-27 07:43:09 +0000

    fix

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: pointer to the destination memory area
+ * @src: pointer to the source memory area
+ * @n: number of bytes to copy
+ *
+ * Return: pointer to dest
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *d = dest;
+	char *s = src;
+
+	while (n--)
+	{
+		*d++ = *s++;
+	}
+
+	return (dest);
+}
+
