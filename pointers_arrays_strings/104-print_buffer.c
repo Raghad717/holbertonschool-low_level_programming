ae1fd1bbf01b981780a02a27128ba6958ef29946
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-01 14:56:23 +0000

    add

--- pointers_arrays_strings/104-print_buffer.c
+++ pointers_arrays_strings/104-print_buffer.c
@@ -8,46 +8,43 @@
  */
 void print_buffer(char *b, int size)
 {
-    int i, j;
+	int i, j;
 
-    if (size <= 0)
-    {
-        printf("\n");
-        return;
-    }
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-    for (i = 0; i < size; i += 10)
-    {
-        /* Print position in hexadecimal */
-        printf("%08x: ", i);
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
 
-        /* Print hexadecimal content (2 bytes at a time) */
-        for (j = i; j < i + 10; j += 2)
-        {
-            if (j < size)
-                printf("%02x", b[j]);
-            else
-                printf("  ");
+		for (j = i; j < i + 10; j += 2)
+		{
+			if (j < size)
+				printf("%02x", b[j]);
+			else
+				printf("  ");
 
-            if (j + 1 < size)
-                printf("%02x ", b[j + 1]);
-            else if (j + 1 < i + 10)
-                printf("   ");
-            else
-                printf(" ");
-        }
+			if (j + 1 < size)
+				printf("%02x ", b[j + 1]);
+			else if (j + 1 < i + 10)
+				printf("   ");
+			else
+				printf(" ");
+		}
 
-        /* Print ASCII content */
-        for (j = i; j < i + 10; j++)
-        {
-            if (j >= size)
-                break;
+		for (j = i; j < i + 10; j++)
+		{
+			if (j >= size)
+				break;
 
-            if (b[j] >= 32 && b[j] <= 126)
-                printf("%c", b[j]);
-            else
-                printf(".");
-        }
-        printf("\n");
-    }
+			if (b[j] >= 32 && b[j] <= 126)
+				printf("%c", b[j]);
+			else
+				printf(".");
+		}
+		printf("\n");
+	}
 }
