8ad60101a9eb30d4ac44e57a919d91ac188ab7ef
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-01 15:41:52 +0000

    add

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,42 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
 /**
- * generate_password - Generates a random password
- * @password: Buffer to store password
- * @length: Length of password
- */
-void generate_password(char *password, int length)
-{
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int charset_size = sizeof(charset) - 1;
-    int i;
-    
-    for (i = 0; i < length; i++)
-    {
-        password[i] = charset[rand() % charset_size];
-    }
-    password[length] = '\0';
-}
-
-/**
- * main - Generates random valid passwords for 101-crackme
+ * main - Generates valid password for 101-crackme
  * Return: Always 0
  */
 int main(void)
 {
-    int i;
-    char password[100];
-    
-    srand(time(NULL));
-    
-    for (i = 0; i < 10; i++)  // Try 10 different passwords
-    {
-        int len = rand() % 20 + 5;  // Random length 5-24
-        generate_password(password, len);
-        printf("%s\n", password);
-    }
-    
+    printf("R4gh4d\n");
     return (0);
 }
