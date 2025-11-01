ef1c7621790d6b4535645116caf5dc94dccc7b8a
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-01 14:52:50 +0000

    add

---
+++ pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - Prints a buffer
+ * @b: Buffer to print
+ * @size: Size of buffer
+ */
+void print_buffer(char *b, int size)
+{
+    int i, j;
+
+    if (size <= 0)
+    {
+        printf("\n");
+        return;
+    }
+
+    for (i = 0; i < size; i += 10)
+    {
+        /* Print position in hexadecimal */
+        printf("%08x: ", i);
+
+        /* Print hexadecimal content (2 bytes at a time) */
+        for (j = i; j < i + 10; j += 2)
+        {
+            if (j < size)
+                printf("%02x", b[j]);
+            else
+                printf("  ");
+
+            if (j + 1 < size)
+                printf("%02x ", b[j + 1]);
+            else if (j + 1 < i + 10)
+                printf("   ");
+            else
+                printf(" ");
+        }
+
+        /* Print ASCII content */
+        for (j = i; j < i + 10; j++)
+        {
+            if (j >= size)
+                break;
+
+            if (b[j] >= 32 && b[j] <= 126)
+                printf("%c", b[j]);
+            else
+                printf(".");
+        }
+        printf("\n");
+    }
+}
