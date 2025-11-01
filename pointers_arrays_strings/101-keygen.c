ee69eac2d8cea58f03a73ef88699d11e855292f2
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-01 15:57:01 +0000

    add

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -8,33 +8,18 @@
  */
 int main(void)
 {
-    int i, j;
-    char password[100];
-    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-    int charset_size = sizeof(charset) - 1;
+    char password[20];
+    int i;
     
     srand(time(NULL));
     
-    /* Generate multiple passwords with different strategies */
-    
-    /* Strategy 1: Simple random passwords */
-    for (i = 0; i < 5; i++)
+    /* Generate exactly one password */
+    for (i = 0; i < 10; i++)
     {
-        int len = rand() % 15 + 8;
-        for (j = 0; j < len; j++)
-        {
-            password[j] = charset[rand() % charset_size];
-        }
-        password[len] = '\0';
-        printf("%s\n", password);
+        password[i] = 'A' + (rand() % 26);
     }
+    password[10] = '\0';
     
-    /* Strategy 2: Try common patterns */
-    printf("password123\n");
-    printf("admin123\n"); 
-    printf("test123\n");
-    printf("holberton\n");
-    printf("school123\n");
-    
+    printf("%s", password);
     return (0);
 }
