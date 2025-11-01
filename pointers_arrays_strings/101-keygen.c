f97cd81b29ed7171b534e64dc48a651edfee35a4
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-01 15:59:25 +0000

    add

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /**
  * main - Generates random valid passwords for 101-crackme
@@ -6,6 +8,18 @@
  */
 int main(void)
 {
-    printf("password123");
+    char password[20];
+    int i;
+    
+    srand(time(NULL));
+    
+    /* Generate exactly one password */
+    for (i = 0; i < 10; i++)
+    {
+        password[i] = 'A' + (rand() % 26);
+    }
+    password[10] = '\0';
+    
+    printf("%s", password);
     return (0);
 }
