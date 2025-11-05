a59f227b0688e1e787362c355250b22cdbf9dd3b
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-05 06:00:47 +0000

    add

--- malloc_free/main.h
+++ malloc_free/main.h
@@ -3,9 +3,14 @@
 
 #include <stdlib.h>
 
-/* Function prototypes */
+/* malloc & free related functions */
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
-#endif
+/* Other utility functions if needed */
+int _putchar(char c);
+
+#endif /* MAIN_H */
