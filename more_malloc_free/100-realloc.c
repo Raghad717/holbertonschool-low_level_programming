7806ad175b9f8932a5a51d5717535eb25fd2fa1a
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-06 20:57:37 +0000

    add

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -1,6 +1,5 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
  * _realloc - reallocates a memory block using malloc and free
@@ -9,18 +8,20 @@
  * @new_size: new size, in bytes of the new memory block
  *
  * Return: pointer to the reallocated memory block
- *         NULL if new_size is zero and ptr is not NULL, or if malloc fails
+ *         NULL if new_size is zero and ptr is not NULL
+ *         NULL if allocation fails
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
-	unsigned int min_size;
+	unsigned int i;
+	char *old_char, *new_char;
 
 	/* If ptr is NULL, equivalent to malloc(new_size) */
 	if (ptr == NULL)
 		return (malloc(new_size));
 
-	/* If new_size is 0 and ptr is not NULL, equivalent to free(ptr) */
+	/* If new_size is zero and ptr is not NULL, equivalent to free(ptr) */
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
@@ -37,9 +38,12 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (NULL);
 
 	/* Copy contents from old block to new block */
-	/* Copy up to the minimum of old_size and new_size */
-	min_size = (old_size < new_size) ? old_size : new_size;
-	memcpy(new_ptr, ptr, min_size);
+	old_char = (char *)ptr;
+	new_char = (char *)new_ptr;
+
+	/* Copy only up to the minimum of old_size and new_size */
+	for (i = 0; i < old_size && i < new_size; i++)
+		new_char[i] = old_char[i];
 
 	/* Free the old memory block */
 	free(ptr);
