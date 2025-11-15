1bc1a57eb354a3a0f41a3c1f3b66c775d6a75f47
Raghad717 <r4gh4dalmalki@gmail.com>
2025-11-15 15:52:48 +0000

    add

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,19 +1,19 @@
 section .data
-    msg db 'Hello, World', 10
-    len equ $ - msg      ; calculate length automatically
+    hello db 'Hello, World', 10
+    hello_len equ $ - hello
 
 section .text
     global _start
 
 _start:
-    ; write system call using int 0x80
-    mov eax, 4          ; sys_write
-    mov ebx, 1          ; stdout
-    mov ecx, msg        ; message pointer
-    mov edx, len        ; message length
-    int 0x80            ; interrupt for system call
+    ; write(1, hello, hello_len)
+    mov rax, 1          ; sys_write
+    mov rdi, 1          ; stdout
+    mov rsi, hello      ; message
+    mov rdx, hello_len  ; length
+    syscall
 
-    ; exit system call
-    mov eax, 1          ; sys_exit
-    mov ebx, 0          ; exit status
-    int 0x80
+    ; exit(0)
+    mov rax, 60         ; sys_exit
+    xor rdi, rdi        ; return 0
+    syscall
