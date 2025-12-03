fb05234d36ba4460c77e54d4b02827315d313b3d
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-03 11:32:05 +0000

    add

--- file_io/main.h
+++ file_io/main.h
@@ -2,16 +2,15 @@
 #define MAIN_H
 
 #include <stddef.h>
-#include <unistd.h>
+#include <sys/types.h>
 
-/* Task 0: read a text file and print it to standard output */
+/* File IO functions */
 ssize_t read_textfile(const char *filename, size_t letters);
-
-/* Task 1: create a file and write text_content to it */
 int create_file(const char *filename, char *text_content);
-
-/* Task 2: append text at the end of a file */
 int append_text_to_file(const char *filename, char *text_content);
+
+/* Copy file program */
+int main(int argc, char *argv[]);
 
 #endif /* MAIN_H */
 
