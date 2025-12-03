c67f1c125d963660a09818e3c8e154af5e310d11
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-03 10:50:16 +0000

    add

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -1,42 +1,39 @@
 #include "main.h"
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 /**
- * create_file - creates a file with specified content
+ * create_file - creates a file and writes text_content to it
  * @filename: name of the file to create
- * @text_content: NULL terminated string to write to the file
+ * @text_content: NULL-terminated string to write to the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t bytes_written = 0;
+	ssize_t w_bytes;
 	size_t len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	/* Open file with required flags and permissions */
-	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (text_content != NULL)
+	{
+		while (text_content[len])
+			len++;
+	}
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
-	/* If text_content is not NULL, write it to the file */
-	if (text_content != NULL)
-	{
-		/* Calculate length of text_content */
-		while (text_content[len])
-			len++;
-
-		/* Write to file */
-		bytes_written = write(fd, text_content, len);
-		if (bytes_written == -1 || (size_t)bytes_written != len)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-
+	w_bytes = write(fd, text_content, len);
 	close(fd);
+
+	if (w_bytes == -1)
+		return (-1);
+
 	return (1);
 }
