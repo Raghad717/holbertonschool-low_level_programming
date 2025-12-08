6a78e4901391aafcca029a25539d4fa42e99448b
Raghad717 <r4gh4dalmalki@gmail.com>
2025-12-08 08:20:53 +0000

    add

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Prints a hash table in order of array, then list
+ * @ht: Hash table
+ *
+ * Format: {'key': 'value', 'key2': 'value2', ...}
+ * Prints nothing if ht is NULL
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	int first = 1; /* Flag for commas */
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
